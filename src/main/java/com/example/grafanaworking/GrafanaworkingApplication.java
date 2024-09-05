6b332fcc8039f01992835df54e698dd0467e5801
patpatpat123 <43899031+patpatpat123@users.noreply.github.com>
2024-09-05 20:44:32 +0800

    Add files via upload

---
+++ src/main/java/com/example/grafanaworking/GrafanaworkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.grafanaworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaworkingApplication.class, args);
+	}
+
+}
